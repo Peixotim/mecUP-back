02164a8a7b2e6f45ee1a73a953ea7f6ba7286624
Peixotim <pedropeixotovz@gmail.com>
2026-07-03 10:26:28 -0300

    feat(database): add base and tenant entity classes

---
+++ src/database/entity/base.entity.ts
@@ -0,0 +1,21 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Index,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('uuid') id!: string
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' }) createdAt!: Date
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' }) updatedAt!: Date
+  @DeleteDateColumn({ name: 'deleted_at', type: 'timestamptz' }) deletedAt?: Date
+}
+
+export abstract class TenantEntity extends BaseEntity {
+  @Index()
+  @Column({ name: 'oficina_id', type: 'uuid' })
+  oficinaId!: string
+}
